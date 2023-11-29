869a5b512a22c903938fb88aaeff97f4f5d98f7e
Kayetic <bartek.bak@protonmail.com>
2023-11-29 13:47:47 +0000

    finished computer science with placeholder topics

--- assets/javascript/computer-science.js
+++ assets/javascript/computer-science.js
@@ -1,6 +1,3 @@
-// open the file "subjects/curriculums/computer-science.txt" and put contents into a variable
-// then split the contents into an array of strings
-
 const curriculum = [
   "1.1.1 Structure and function of the processor (A Level only)",
   "1.1.1 Structure and function of the processor",
@@ -12,12 +9,53 @@ const curriculum = [
   "1.2.3 Software Development",
 ];
 
-// pick a random index from the array and assign that to a variable called chosen-topic
+const applyEffects = function () {
+  const topic = document.querySelector(".topic");
+  topic.style.filter = "blur(20px)";
+  topic.style.transform = "scale(0.1)";
+  topic.style.opacity = "0.3";
+};
 
-const chosenTopic = curriculum[Math.floor(Math.random() * curriculum.length)];
-// get the element with class "topic" and assign that to a variable called topic
+const removeEffects = function () {
+  const topic = document.querySelector(".topic");
+  topic.style.filter = "blur(0)";
+  topic.style.transform = "scale(1)";
+  topic.style.opacity = "1";
+};
 
-const topic = document.querySelector(".topic");
-// set the text of the element with class "topic" to the value of the variable chosen-topic
+const randomTopic = function (curriculumArray, isFirstLoad = True) {
+  const topic = document.querySelector(".topic");
 
-topic.innerText = chosenTopic;
+  // Apply blur and scale down before changing the text
+  applyEffects();
+
+  // if is first load assign text immediately, no timeout but still apply effects
+  if (isFirstLoad) {
+    const topic = document.querySelector(".topic");
+    topic.style.filter = "blur(20px)";
+
+    topic.innerText =
+      curriculumArray[Math.floor(Math.random() * curriculumArray.length)];
+    setTimeout(removeEffects, 10);
+    // removeEffects();
+  } else {
+    setTimeout(function () {
+      topic.innerText =
+        curriculumArray[Math.floor(Math.random() * curriculumArray.length)];
+    }, 300);
+    setTimeout(removeEffects, 500);
+  }
+};
+
+// load a random topic on page load
+randomTopic(curriculum, true);
+setTimeout(function () {
+  const rerollButton = document.querySelector(".reroll");
+  // rerollButton.style.display = "block";
+  rerollButton.style.opacity = "1";
+  rerollButton.style.filter = "blur(0)";
+}, 500);
+
+document.querySelector(".reroll").addEventListener("click", function () {
+  randomTopic(curriculum, false);
+});
