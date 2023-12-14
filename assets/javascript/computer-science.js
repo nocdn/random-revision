694147016b46d93e2533c4047cfca0877a9f6083
Kayetic <bartek.bak@protonmail.com>
2023-12-14 21:16:28 +0000

    fixed curriculum

--- assets/javascript/computer-science.js
+++ assets/javascript/computer-science.js
@@ -1,7 +1,5 @@
 const curriculum = [
-  "1.1.1 Structure and function of the processor (A Level only)",
   "1.1.1 Structure and function of the processor",
-  "1.1.2 Types of Processor (A Level only)",
   "1.1.2 Types of Processor",
   "1.1.3 Input, Output and Storage",
   "1.2.1. Systems Software",
